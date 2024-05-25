4b2d50d7a2cfb6ecfcc1b330351e9442ff0032a3
Mihail-lozovoy <lozovoy_michael@inbox.ru>
2024-05-25 16:18:19 +0300

    Add files via upload

---
+++ homework5.1.py
@@ -0,0 +1,4 @@
+mutable_list = [1 ,2 ,True ,"String"]
+print(mutable_list)
+mutable_list[0]=5
+print(mutable_list)
---
+++ homework5.py
@@ -0,0 +1,4 @@
+immutable_var = 1, 2, 3, True, "String"
+print(immutable_var)
+immutable_var [0]=5
+print(immutable_var)
